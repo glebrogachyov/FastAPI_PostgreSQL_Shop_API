83d882bee1a9a7167c1af74181708e870bbcf336
Gleb Rogachyov <33635536+glebrogachyov@users.noreply.github.com>
2023-02-14 02:47:23 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,13 +22,13 @@ def get_db():
         db.close()
 
 
-@app.get("/")
+@app.get("/", tags=["Redirect to OpenAPI docs"])
 def root() -> RedirectResponse:
     """ Перенаправление на страницу документации OpenAPI """
     return RedirectResponse(url="http://localhost:8000/docs")
 
 
-@app.post("/init_tables")
+@app.post("/init_tables", tags=["Tables"], name="Проинициализировать таблицы товаров и корзины")
 def init_tables(response: Response,
                 create_empty: bool = Query(default=False, description="Создать без записей"),
                 db=Depends(get_db)):
@@ -41,7 +41,7 @@ def init_tables(response: Response,
     return result["response"]
 
 
-@app.delete("/drop_tables")
+@app.delete("/drop_tables", tags=["Tables"], name="Удалить таблицы товаров и корзины")
 def drop_tables(response: Response):
     """ Удалить таблицы и данные """
     result = crud.drop_tables()
@@ -49,7 +49,7 @@ def drop_tables(response: Response):
     return result["response"]
 
 
-@app.get("/products", response_model=list[BaseProduct] | dict)
+@app.get("/products", response_model=list[BaseProduct] | dict, tags=["Products"], name="Вывести товары в базе данных")
 def search_products(response: Response,
                     names: set[str] | None = Query(default=None, max_length=50, title="Search keywords"),
                     min_price: float | None = Query(default=None, gt=0),
@@ -63,7 +63,7 @@ def search_products(response: Response,
     return result["response"]
 
 
-@app.get("/products/{product_id}", response_model=DetailedProductIn | dict)
+@app.get("/products/{product_id}", response_model=DetailedProductIn | dict, tags=["Products"], name="Описание товара")
 def get_product_description(response: Response, product_id: int, db=Depends(get_db)):
     """ Получить детальное описание товара по его идентификатору """
     result = crud.get_product_description(db, product_id)
@@ -71,7 +71,7 @@ def get_product_description(response: Response, product_id: int, db=Depends(get_
     return result["response"]
 
 
-@app.get("/cart")
+@app.get("/cart", tags=["Cart"], name="Вывести список товаров в корзине")
 def list_cart(response: Response, db=Depends(get_db)):
     """ Получить всю корзину """
     result = crud.list_cart(db)
@@ -79,7 +79,7 @@ def list_cart(response: Response, db=Depends(get_db)):
     return result["response"]
 
 
-@app.post("/products")
+@app.post("/products", tags=["Products"], name="Создать новый товар")
 def create_product(response: Response, product: DetailedProductIn, db=Depends(get_db)):
     """ Создать новый товар """
     result = crud.create_product(db, product)
@@ -87,7 +87,7 @@ def create_product(response: Response, product: DetailedProductIn, db=Depends(ge
     return result["response"]
 
 
-@app.delete("/products/{product_id}")
+@app.delete("/products/{product_id}", tags=["Products"], name="Удалить товар из базы данных")
 def remove_product(response: Response, product_id: int, db=Depends(get_db)):
     """ Удалить существующий товар """
     result = crud.remove_product(db, product_id)
@@ -95,7 +95,7 @@ def remove_product(response: Response, product_id: int, db=Depends(get_db)):
     return result["response"]
 
 
-@app.patch("/cart")
+@app.patch("/cart", tags=["Cart"], name="Добавить товар в корзину или изменить количество (0 = удалить из корзины)")
 def add_to_cart(response: Response, product_to_cart: ProductToCart, db=Depends(get_db)):
     """ Добавить товар в корзину, поменять количество товара в корзине. Количество = 0 уберёт товар из корзины """
     result = crud.add_to_cart(db, product_to_cart)
