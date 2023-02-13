81372ad5cbf2d424c96a56c1662c23ccb3a26e7a
Gleb Rogachev <glebrogachyov@yandex.ru>
2023-02-13 18:08:38 +0300

    working version

--- crud.py
+++ crud.py
@@ -1,29 +1,41 @@
+from sqlalchemy import desc, or_
 from sqlalchemy.orm import Session
 
 import models
 import schemas
+from database import engine
 
-"""
-Нужны методы:
-        1) Создать новый товар (минимальное описание обязательно - имя, цена, остальное опционально)
-        2) Удалить существующий товар 
-    3) Вывести все товары с фильтрацией и сортировкой (но это опционально, по дефолту выводить как есть)
-        4) Добавить товар в корзину в нужном кол-ве (если кол-во 0, значит удалить)
-    5) Получить всю корзину (минимальное описание товаров + кол-во, а также стоимость всей корзины)
-    6) 
-"""
+
+def filtrate(query, names=None, min_price=None, max_price=None, name_order_by=None, price_order_by=None):
+    if names:
+        query = query.filter(or_(*[models.Product.name.like(f"%{name_.lower()}%") for name_ in names]))
+
+    if min_price:
+        query = query.filter(models.Product.price >= min_price)
+    if max_price:
+        query = query.filter(models.Product.price <= max_price)
+
+    if name_order_by == "asc":
+        query = query.order_by(models.Product.name)
+    if name_order_by == "desc":
+        query = query.order_by(desc(models.Product.name))
+
+    if price_order_by == "asc":
+        query = query.order_by(models.Product.price)
+    if price_order_by == "desc":
+        query = query.order_by(desc(models.Product.price))
+    return query
 
 
 def create_product(db: Session, product: schemas.DetailedProductIn):
     try:
-        print(f"adding new product: {product}")
         db_product = models.Product(name=product.name, price=product.price, description=product.description)
-        print(f"product db repr: {db_product}")
         db.add(db_product)
         db.commit()
     except Exception as e:
-        return {"message": f"error. {e}"}
-    return {"message": f"created product: {db_product.name} price: {db_product.price}"}
+        return {"status_code": 500, "response": {"message": f"error. debug: {e}"}}
+    return {"status_code": 201,
+            "response": {"message": f"created product: {db_product.name} | price: {db_product.price}"}}
 
 
 def remove_product(db: Session, product_id: int):
@@ -33,49 +45,46 @@ def remove_product(db: Session, product_id: int):
             db.delete(db_product)
             db.commit()
         else:
-            return {"message": f"no item with id {product_id}"}
+            return {"status_code": 404, "response": {"message": f"no item with id {product_id}"}}
     except Exception as e:
-        return {"message": "database error. " + repr(e)}
-    return {"message": f"removed {db_product.name} {db_product.price}"}
+        return {"status_code": 500, "response": {"message": "database error. debug: " + repr(e)}}
+    return {"status_code": 200, "response": {"message": f"removed {db_product.name}; price: {db_product.price}"}}
 
 
-def list_products(db: Session,
-                  names_filter_list=None,
-                  min_price=None,
-                  max_price=None,
-                  name_order_by=None,
-                  price_order_by=None):
+def list_products(db: Session, names=None, min_price=None, max_price=None, name_order_by=None, price_order_by=None):
     try:
-        # Добавить фильтрацию
-        db_products = db.query(models.Product).filter(models.Product.id > 0).all()
-        print(db_products)
+        query = filtrate(db.query(models.Product), names=names, min_price=min_price, max_price=max_price,
+                         name_order_by=name_order_by, price_order_by=price_order_by)
+        db_products = query.all()
+
     except Exception as e:
-        return {"message": "database error. " + repr(e)}
-    return db_products
+        return {"status_code": 500, "response": {"message": "database error. debug: " + repr(e)}}
+    return {"status_code": 200, "response": db_products}
 
 
 def get_product_description(db: Session, product_id):
     try:
         db_product = db.query(models.Product).filter(models.Product.id == product_id).first()
+        print(f"{db_product=}")
         if not db_product:
-            return {"message": f"no item with id {product_id}"}
+            print("not db product")
+            return {"status_code": 404, "response": {"message": f"no item with id {product_id}"}}
     except Exception as e:
-        return {"message": "database error. " + repr(e)}
-    return db_product
+        return {"status_code": 500, "response": {"message": "database error. debug: " + repr(e)}}
+    return {"status_code": 200, "response": db_product}
 
 
 def add_to_cart(db: Session, product):
     try:
         is_product_exist = db.query(models.Product).filter(models.Product.id == product.id).first()
         if not is_product_exist:
-            return {"message": f"product with id {product.id} doesn't exist"}
-        # Дописать проверку на наличие товара в тележке: если нет, тогда добавить, если не ноль, иначе - изменить кол-во
+            return {"status_code": 404, "response": {"message": f"product with id {product.id} doesn't exist"}}
 
         is_product_in_cart = db.query(models.Cart).filter(models.Cart.product_id == product.id).first()
         if not is_product_in_cart:
             if product.amount == 0:
                 print(db, product.id)
-                return {"message": f"no such product with id {product.id} in cart. nothing to remove."}
+                return {"status_code": 404, "response": {"message": f"no product with id {product.id} in cart."}}
             cart_product = models.Cart(product_id=product.id, amount=product.amount)
             db.add(cart_product)
         else:
@@ -85,18 +94,36 @@ def add_to_cart(db: Session, product):
                 is_product_in_cart.amount = product.amount
         db.commit()
     except Exception as e:
-        return {"message": f"error adding product with id {product.id} to cart. {repr(e)}"}
-    return {"message": f"done. {product.id=} amount set to {product.amount=}"}
+        return {"status_code": 500,
+                "response": {"message": f"error adding product with id {product.id} to cart. debug: {repr(e)}"}}
+    return {"status_code": 200,
+            "response": {"message": f"done. product with id {product.id} amount set to {product.amount}"}}
 
 
 def list_cart(db: Session):
     try:
-        t_c, t_p = models.Cart, models.Product
-        query = db.query(t_p.id, t_p.name, t_p.price, t_c.amount).select_from(t_c).join(t_p, t_c.product_id == t_p.id)
+        crt, prd = models.Cart, models.Product
+        query = db.query(prd.id, prd.name, prd.price, crt.amount).select_from(crt).join(prd, crt.product_id == prd.id)
         cart_items = query.all()
         keys_ = schemas.ProductInCart.__fields__.keys()
         for idx, item in enumerate(cart_items):
             cart_items[idx] = schemas.ProductInCart(**{k: v for k, v in zip(keys_, item)})
     except Exception as e:
-        return {"message": "database error. " + repr(e)}
-    return cart_items
+        return {"status_code": 500, "response": {"message": "database error. debug: " + repr(e)}}
+    return {"status_code": 200, "response": schemas.Cart(items=cart_items)}
+
+
+def init_tables():
+    try:
+        models.Base.metadata.create_all(bind=engine)
+    except Exception as e:
+        return {"status_code": 500, "response": {"message": "database error. debug: " + repr(e)}}
+    return {"status_code": 200, "response": {"message": "tables created"}}
+
+
+def drop_tables():
+    try:
+        models.Base.metadata.drop_all(bind=engine)
+    except Exception as e:
+        return {"status_code": 500, "response": {"message": "database error. debug: " + repr(e)}}
+    return {"status_code": 200, "response": {"message": "tables dropped"}}
